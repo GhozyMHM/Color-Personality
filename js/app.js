1b2ddce227ace33bee4ba9a20a4f6f37827714b6
Ghozy Muhammad Hanzhalah Muchjidi <gliozghozy@gmail.com>
2020-09-03 13:02:00 +0700

    Update Navbar Color Changing On Scroll

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 import "./component/nav-bar.js";
 import "./component/title-content.js";
+
+var navbar = document.getElementsByClassName('navig')[0]
+
+window.onscroll = function () {
+    if (window.pageYOffset > 10) {
+        navbar.style.background = "rgb(201, 23, 201)";
+    } else {
+        navbar.style.background = "transparent";
+    }
+}
