636c11209e346a852afb5fefdddc9d950301beaf
Ghozy Muhammad Hanzhalah Muchjidi <gliozghozy@gmail.com>
2020-09-04 13:01:23 +0700

    Update Navbar Changing Color

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var navbar = document.getElementsByClassName('navig')[0]
 
 window.onscroll = function () {
     if (window.pageYOffset > 10) {
-        navbar.style.background = "rgb(201, 23, 201)";
+        navbar.style.background = "rgba(201, 23, 201, 0.685)";
     } else {
         navbar.style.background = "transparent";
     }
